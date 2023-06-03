20d123790eaeb2eab6a1212ac427ae1b52536d21
Ruslan Zainetdinov <manfuckschool@mail.ru>
2023-06-03 22:26:09 +0300

    Работает!!!!

--- main.py
+++ main.py
@@ -39,60 +39,68 @@ def check_e_condition(link_vertex):
 
 
 def step(start_vertex, graph_structure):  # проходим по ребру, после удаляем его
-    print("start", start_vertex)
+    # print("start", start_vertex)
+
     # берём список соседей начальной вершины
     neighbours_of_start_vertex = graph_structure.get(start_vertex)
-    print('start neighbours', neighbours_of_start_vertex)
+    # print('start neighbours', neighbours_of_start_vertex)
 
     finish_vertex = random.choice(neighbours_of_start_vertex)
-    print('finish', finish_vertex)
-
     # берём список соседей конечной вершины
     neighbours_of_finish_vertex = graph_structure.get(finish_vertex)
-    print('finish neighbours', neighbours_of_finish_vertex)
+    # print('finish neighbours', neighbours_of_finish_vertex)
 
-    if len(neighbours_of_finish_vertex) > 1:
-        if finish_vertex != start:
-            if finish_vertex:
-                # удаляем конечную вершину из соседей начальной вершины
-                neighbours_of_start_vertex.remove(finish_vertex)
-                # удаляем начальную вершину из соседей конечной вершины
-                neighbours_of_finish_vertex.remove(start_vertex)
-
-                # обновляем граф, учитывая удалённое ребро
-                graph_structure.update({start_vertex: neighbours_of_start_vertex})
-                graph_structure.update({finish_vertex: neighbours_of_finish_vertex})
-
-                print(f'Из {start_vertex} в {finish_vertex}')
-
-        else:
-            print(f"Вершина {finish_vertex} совпадает с {start}")
-            return step(start_vertex, graph_structure)
+    # если конечная вершина не единственный сосед или это мост, то заново выбираем конечную точку
+    if ((finish_vertex == start) and (len(neighbours_of_start_vertex) > 1)) or \
+            ((len(neighbours_of_finish_vertex) == 1) and (alive_egde_count() > 1)):
+        step(start_vertex, graph_structure)
     else:
-        print("Осталась конечная вершина, ", finish_vertex)
+        # if len(neighbours_of_finish_vertex) > 0 and (alive_egde_count() > 0):
+            # if finish_vertex:
+        # удаляем конечную вершину из соседей начальной вершины
+        neighbours_of_start_vertex.remove(finish_vertex)
+        # удаляем начальную вершину из соседей конечной вершины
+        neighbours_of_finish_vertex.remove(start_vertex)
+
+        # обновляем граф, учитывая удалённое ребро
+        graph_structure.update({start_vertex: neighbours_of_start_vertex})
+        graph_structure.update({finish_vertex: neighbours_of_finish_vertex})
+
+        print(f'Из {start_vertex} в {finish_vertex}')
+
+        # else:
+        #     print("Осталась конечная вершина, ", finish_vertex)
+    print(start_vertex)
     print(finish_vertex)
     return graph_structure, finish_vertex
 
 
+# считаем количество оставшихся рёбер
+def alive_egde_count():
+    e_c = 0
+    for key in l_v.keys():
+        if l_v[key]:
+            e_c += 1
+            # print("edges: ", key, l_v[key])
+    return e_c//2
+
+
 def built_circle(base_graph):
     global start
-    if base_graph[0] == 'eiler':  # если граф эйлеровый
-        lst_v = [i for i in base_graph[1].keys()]  # составляем список вершин
-        # start = random.choice(lst_v)  # выбираем случайную вершину
-        start = c
-        graph = base_graph[1].copy()  # для удобства работы создаём словарь граф, с которым и будем работать
-        graph, current_vertex = step(start, graph)  # проходим одну итерацию по графу
-        way_v = [start, current_vertex]  # создаём список, куда последовательно будем добавлять пройденные вершины
+    lst_v = [i for i in base_graph[1].keys()]  # составляем список вершин
+    start = random.choice(lst_v)  # выбираем случайную вершину
+    # start = c
+    graph = base_graph[1].copy()  # для удобства работы создаём словарь граф, с которым и будем работать
+    graph, current_vertex = step(start, graph)  # проходим одну итерацию по графу
+    way_v = [start, current_vertex]  # создаём список, куда последовательно будем добавлять пройденные вершины
 
-        while current_vertex != start:
-            graph, current_vertex = step(current_vertex, graph)
-            way_v.append(current_vertex)
-        return 'Граф является эйлеровым, вот его маршрут: ', way_v
-
-    elif base_graph[0] == 'semi-eiler':  # если граф полуэйлеровый
-        pass
-    else:
-        return 'Граф не является Эйлеровым, невозможно использовать алгоритм Флери'
+    edge_count = alive_egde_count()
+    while edge_count > 0:
+        graph, current_vertex = step(current_vertex, graph)
+        way_v.append(current_vertex)
+        edge_count = alive_egde_count()
+        # print(edge_count)
+    return 'Граф является эйлеровым, вот его маршрут: ', way_v
 
 
 def built_way(base_graph):
@@ -115,6 +123,11 @@ l_v = {
 
 is_graph_e = check_e_condition(l_v)
 # print(l_v)
-print(built_circle(is_graph_e))
-# print(built_way(is_graph_e))
+if is_graph_e[0] == 'eiler':  # если граф эйлеровый:
+    print(*built_circle(is_graph_e))
+elif is_graph_e[0] == 'semi-eiler':  # если граф полуэйлеровый
+    print(built_way(is_graph_e))
+else:
+    print('Граф не является Эйлеровым, невозможно использовать алгоритм Флёри')
+
 
